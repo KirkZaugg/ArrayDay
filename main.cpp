d4202f09e88949b8e72c5a5c75114325402c8b5d
KirkZaugg <25kzaugg@go.dsdmail.net>
2024-09-27 13:43:02 +0000

    finish

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 
 void dispArray(int in[3][3]) {
-    int sum
+    int sum = 0;
+    int product = 1;
     std::cout << "Matrix:\n";
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
             std::cout << in[i][j];
+            sum+=in[i][j];
+            product*=in[i][j];
         }
         std::cout << "\n";
     }
-
+    std::cout << "The sum is " << sum << "\nThe average is " << sum/9 << "\nThe product is " << product << "\n";
 }
 
 int main() {
